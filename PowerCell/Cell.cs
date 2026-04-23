fb3c903359d60cbec5f466bb677fc5e8ec9f78bd
Miguel Martins <miguelarbmartins@gmail.com>
2026-04-23 16:03:24 +0100

    Class Cell.cs

---
+++ PowerCell/Cell.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Globalization;
+
+namespace PowerCell
+{
+    public class Cell
+    {
+        private float charge;
+
+        public string Name { get; }
+
+        public float Charge
+        {
+            get { return charge; }
+            set
+            {
+                if (value < 0) charge = 0;
+                else if (value > 200) charge = 200;
+                else charge = value;
+            }
+        }
+
+        public int Level
+        {
+            get { return 1 + (int)(Charge / 40); }
+        }
+
+        public Cell(string name)
+        {
+            Name = name;
+            Charge = 200;
+        }
+
+        public void Consume(float amount)
+        {
+            Charge -= amount;
+        }
+
+        public void Restore()
+        {
+            Charge = 200;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Name}] Level {Level}: {Charge}/200";
+        }
+    }
+}
